9d9981804317e86c75cfdc2e61761466382507a0
MaltNoodlez <maltnoodlez@gmail.com>
2026-02-05 13:03:45 +0100

    Functions '/getTrain' & '/getCatFact' completed and ready-to-use

--- Server/index.js
+++ Server/index.js
@@ -5,42 +5,62 @@ require('dotenv').config();
 const API = process.env.NS_API;
 app.use(express.json());
 
-const city_name = "Schiphol";
-const url = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations?q=${city_name}&limit=1`;
-const catfact = `https://meowfacts.herokuapp.com/?lang=eng`
+let cityName = "Enschede";
+
+const cityInfoUrl = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations?q=${cityName}&limit=1`;
+const catfact = `https://meowfacts.herokuapp.com/?lang=eng&count=3`
 
 async function getStations() {
-  const response = await fetch(url, {
+
+  const response = await fetch(cityInfoUrl, {
+    headers: { "Ocp-Apim-Subscription-Key": API }
+  });
+  const data = await response.json();
+  console.log(data.payload[0])
+  const cityCode = data.payload[0].UICCode;
+
+  const cityDeparturesUrl = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?lang=en&uicCode=${cityCode}&maxJourneys=5`;
+
+  const departures = await fetch(cityDeparturesUrl, {
     headers: { "Ocp-Apim-Subscription-Key": API }
   });
 
-  const data = await response.json();
+  const departureData = await departures.json();
+  console.log(departureData.payload.departures);
 
-  console.log(data.payload[0]);
-  return data.payload[0].sporen;
+  return departureData;
 }
 
 async function catFact() {
     const catFactResponse = await fetch(catfact)
     const catData = await catFactResponse.json();
 
-    console.log(catData.data[0]);
+    console.log(catData.data);
+    return catData.data
 }
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-  
-  catFact();
-  getStations();
 });
 
-app.get('/', async (req, res) => {
-  try {
-    const data = await getStations();
-    console.log(data);
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch station data' });
-  }
+app.get('/getTrain', async (req, res) => {
+    try {
+        const data = await getStations();
+        console.log(data);
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch station data '});
+    }
+});
+
+app.get('/getCatFact', async (req, res) => {
+    try {
+        const catFactList = await catFact();
+        console.log(catFactList);
+        res.status(200).json(catFactList);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch cat fact :( '});
+    }
 });
