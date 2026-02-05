e0643ac3db3c5f99fe6a8ae3ddb199eb2d49dcdc
MaltNoodlez <maltnoodlez@gmail.com>
2026-02-05 11:04:46 +0100

    Added basic node code to read from the API

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,37 @@
-const express= require('express')
-const app= express()
-const PORT = 3000
+const express = require('express');
+const app = express();
+const PORT = 3000;
+require('dotenv').config();
+const API = process.env.NS_API;
+app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log(`App listening on port ${PORT}`)
-})
+const city_name = "Enschede";
+const url = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations?q=${city_name}&limit=1`;
+
+async function getStations() {
+  const response = await fetch(url, {
+    headers: { "Ocp-Apim-Subscription-Key": API }
+  });
+
+  const data = await response.json();
+
+  console.log(data.payload[0].namen);
+  return data.payload[0].namen;
+}
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+
+  getStations();
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await getStations();
+    console.log(data);
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch station data' });
+  }
+});
