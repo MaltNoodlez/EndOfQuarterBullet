f454beb8f4659f9e669d018cc335e41fa224c1df
Blauwemangogirl <aliciamoen@icloud.com>
2026-02-06 14:35:20 +0100

    radio fix api key

--- server/index.js
+++ server/index.js
@@ -1,67 +1,69 @@
-const express = require('express');
-const cors= require('cors')
-const {getLocalRadio} = require('./provinceRadio')
+const express = require("express");
+const cors = require("cors");
+const { getLocalRadio } = require("./provinceRadio");
 const app = express();
 const PORT = 3001;
-require('dotenv').config();
+require("dotenv").config();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-const API = process.env.NS_API || 'b9b6ca97eaff4e34abaa3843eebf8984';
+const API = process.env.NS_API || "b9b6ca97eaff4e34abaa3843eebf8984";
 const WORLD_API = process.env.STATE_API;
-const COUNTRY_ISO2 = "NL"
+const COUNTRY_ISO2 = "NL";
 
-const catfact = `https://meowfacts.herokuapp.com/?lang=eng&count=3`
-
+const catfact = `https://meowfacts.herokuapp.com/?lang=eng&count=3`;
 
 async function getStations(cityName) {
-    const cityInfoUrl = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations?q=${cityName}&limit=1`;
+  const cityInfoUrl = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations?q=${cityName}&limit=1`;
 
-    const response = await fetch(cityInfoUrl, {
-    headers: { "Ocp-Apim-Subscription-Key": API }
-    });
-    const data = await response.json();
-    console.log(data.payload[0])
-    const cityCode = data.payload[0].UICCode;
+  const response = await fetch(cityInfoUrl, {
+    headers: { "Ocp-Apim-Subscription-Key": API },
+  });
+  const data = await response.json();
+  console.log(data.payload[0]);
+  const cityCode = data.payload[0].UICCode;
 
-    const cityDeparturesUrl = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?lang=en&uicCode=${cityCode}&maxJourneys=5`;
+  const cityDeparturesUrl = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?lang=en&uicCode=${cityCode}&maxJourneys=5`;
 
-    const departures = await fetch(cityDeparturesUrl, {
-    headers: { "Ocp-Apim-Subscription-Key": API }
-    });
+  const departures = await fetch(cityDeparturesUrl, {
+    headers: { "Ocp-Apim-Subscription-Key": API },
+  });
 
-    const departureData = await departures.json();
-    return departureData;
+  const departureData = await departures.json();
+  return departureData;
 }
 
 async function catFact() {
-    const catFactResponse = await fetch(catfact)
-    const catData = await catFactResponse.json();
-    return catData.data
+  const catFactResponse = await fetch(catfact);
+  const catData = await catFactResponse.json();
+  return catData.data;
 }
 
 async function lookUpProvince(cityName) {
   const response = await fetch(
     `https://api.countrystatecity.in/v1/countries/${COUNTRY_ISO2}/states`,
-    { headers: { 'X-CSCAPI-KEY': WORLD_API } }
+    { headers: { "X-CSCAPI-KEY": WORLD_API } }
   );
   const provinces = await response.json();
+  console.log(provinces)
 
   const cityPromises = provinces.map(async (province) => {
     const res = await fetch(
       `https://api.countrystatecity.in/v1/countries/${COUNTRY_ISO2}/states/${province.iso2}/cities`,
-      { headers: { 'X-CSCAPI-KEY': WORLD_API } }
+      { headers: { "X-CSCAPI-KEY": WORLD_API } }
     );
     const cities = await res.json();
 
-    const match = cities.find(c => c.name.toLowerCase() === cityName.toLowerCase());
+    const match = cities.find(
+      (c) => c.name.toLowerCase() === cityName.toLowerCase()
+    );
     if (match) return province.name;
     return null;
   });
 
   const results = await Promise.all(cityPromises);
 
-  const provinceFound = results.find(p => p !== null);
+  const provinceFound = results.find((p) => p !== null);
 
   if (provinceFound) {
     console.log({ city: cityName, province: provinceFound });
@@ -76,38 +78,38 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}  `);
 });
 
-app.post('/getTrain', async (req, res) => {
-    const {city}=req.body
-    
-    try {
-        const data = await getStations(city);
-        console.log('error')
-        console.log(data);
-        res.status(200).json(data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to fetch station data '});
-    }
+app.post("/getTrain", async (req, res) => {
+  const { city } = req.body;
+
+  try {
+    const data = await getStations(city);
+    console.log("error");
+    console.log(data);
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch station data " });
+  }
 });
 
-app.get('/getCatFact', async (req, res) => {
-    try {
-        const catFactList = await catFact();
-        res.status(200).json(catFactList);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to fetch cat fact :( '});
-    }
+app.get("/getCatFact", async (req, res) => {
+  try {
+    const catFactList = await catFact();
+    res.status(200).json(catFactList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch cat fact :( " });
+  }
 });
 
-app.post('/radio', async (req, res) => {
-    const {city}=req.body
-    try {
-        const currentProvince = await lookUpProvince(city);
-        const localRadio = getLocalRadio(currentProvince['province'])
-        res.status(200).json(localRadio, currentProvince);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to fetch province data'})
-    }
+app.post("/radio", async (req, res) => {
+  const { city } = req.body;
+  try {
+    const currentProvince = await lookUpProvince(city);
+    const localRadio = getLocalRadio(currentProvince["province"]);
+    res.status(200).json(localRadio, currentProvince);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch province data" });
+  }
 });
