44661b35fe516a1d29b773f3fd6b41bf4abf49ee
david <582197@student.saxion.nl>
2026-02-06 11:14:25 +0100

    Working version2

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -2,12 +2,12 @@ import screenView from '@/views/screenView.vue'
 import StartView from '@/views/startView.vue'
 import stationView from '@/views/stationView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import GameView from '@/views/GameView.vue'
+
 const routes = [
   { path: '/start', component: StartView },
   { path: '/screen', component: screenView },
-  { path: '/', component: stationView },
-  // { path: '/', component: GameView },
+  { path: '/rotterdam', component: stationView },
+  
     
 ]
 
